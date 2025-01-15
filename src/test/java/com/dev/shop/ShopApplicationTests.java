559016a957faccc87afd662e897e033dd3a5ded6
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2025-01-15 10:51:20 +0700

    A demo project for e-commerce app

---
+++ src/test/java/com/dev/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
